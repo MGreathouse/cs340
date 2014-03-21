af84f55339b228d01c79e4389e255ae1377a261a
Marcus E Greathouse <mgreathouse@cslab004.olivetcollege.edu>
2014-03-21 12:38:38 -0400

    adding functionality

--- in-class/linkedList.c
+++ in-class/linkedList.c
@@ -1,3 +1,8 @@
+/*
+    min, max, sum
+*/
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -6,7 +11,11 @@ typedef struct node {
     struct node *next;
 } Node;
 
-void createNode(int value, Node **head, Node **tail);
+Node* createNode(int value);
+void insertAt(int value, Node **head, Node **tail, int loc);
+void append(Node *node, Node **head, Node **tail);
+int count(Node *head);
+int pop(Node **head);
 void printNodes(Node *head);
 
 
@@ -14,28 +23,54 @@ int main(void) {
     Node *head = NULL, *tail = NULL;
     int i;
 
-    int numbers[10];
     for(i = 0; i < 10; i++) {
-        numbers[i] = i + 1;
-    }
-
-    for(i = 0; i < 10; i++) {
-        createNode(numbers[i], &head, &tail);
+        append(createNode(i+1), &head, &tail);
     }
 
     printNodes(head);
+    printf("\nThere are %d Nodes in this linked list.\n\n", count(head));
+
+    insertAt(11, &head, &tail, 5);
+
+    printNodes(head);
+    printf("\nThere are %d Nodes in this linked list.\n\n", count(head));
+
+    insertAt(12, &head, &tail, 0);
+
+    printNodes(head);
+    printf("\nThere are %d Nodes in this linked list.\n\n", count(head));
+
+    insertAt(13, &head, &tail, 15);
+
+    printNodes(head);
+    printf("\nThere are %d Nodes in this linked list.\n\n", count(head));
+
+    insertAt(14, &head, &tail, 1);
+
+    printNodes(head);
+    printf("\nThere are %d Nodes in this linked list.\n\n", count(head));
+
+    printf("\nYou popped: %d\n\n", pop(&head));
+
+    printNodes(head);
+    printf("\nThere are %d Nodes in this linked list.\n\n", count(head));
 
     return 0;
 }
 
 
-void createNode(int value, Node **head, Node **tail) {
+Node* createNode(int value) {
     Node *node;
 
     node = (Node *) malloc(sizeof(Node));
     node -> data = value;
     node -> next = NULL;
 
+    return node;
+}
+
+
+void append(Node *node, Node **head, Node **tail) {
     if(*tail != NULL) {
         (*tail) -> next = node;
     }
@@ -46,6 +81,49 @@ void createNode(int value, Node **head, Node **tail) {
         *head = node;
     }
 }
+
+
+void insertAt(int value, Node **head, Node **tail, int loc) {
+    int cnt = count(*head);
+    Node *node = createNode(value);
+
+    if (cnt < loc || loc < 0) {
+        append(node, head, tail);
+    } else if (loc == 0) {
+        node -> next = *head;
+        *head = node;
+    } else {
+        Node *ptr = *head;
+
+        while (--loc > 0) {
+            ptr = ptr -> next;
+        }
+
+        node -> next = ptr -> next;
+        ptr -> next = node;
+    }
+}
+
+
+int count(Node *head) {
+    Node *ptr = head;
+    int cnt = 0;
+    
+    while (ptr != NULL) {
+        cnt++;
+        ptr = ptr -> next;
+    }
+    return cnt;
+}
+
+
+int pop(Node **head) {
+    int val =  (*head) -> data;
+    *head = (*head) -> next;
+    
+    return val;
+}
+
 
 void printNodes(Node *head) {
     Node *ptr = head;
